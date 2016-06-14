8f7fecd03f91ece727a1e4ed50df63595a330cc6
AndyCodez <andystudy4@gmail.com>
2016-06-14 12:44:19 +0300

    Complete basic structure of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'about' => 'static_pages#about'
+
+  get 'help' => 'static_pages#help'
+
+  get 'contact' => 'static_pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
